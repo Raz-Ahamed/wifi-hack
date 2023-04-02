9c272a8edcba6efd623ca2e32601b248e3896eef
Raz Ahamed <100101605+Raz-Ahamed@users.noreply.github.com>
2023-04-02 10:35:27 +0600

    Add files via upload

---
+++ wifi.py
@@ -0,0 +1,172 @@
+#python 
+#Youtube chanel name: Real tips & trikes 660
+#wifi_hack 
+#wps_hack 
+#start 
+import os
+print('Chack Update')
+os.system('git pull')
+os.system('clear')
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+import  os 
+os.system('clear')
+os.system('figlet -f big Wifi - Hack |lolcat')
+print(green+'                     Version : 1.0 ')
+print()
+print(blue+'======================================================')
+print(red+'Note: I wont be responsible fo any illigal activites.')
+print(blue+'======================================================')
+print()
+#main.coding
+#________________________________________
+import os
+import sys
+import time
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.10)
+delay_print(green+'%37s' % '   |Devoloped By : Raz Ahamed|\n')
+print()
+print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(blue+'Youtube Chanel: Real  tips and Trikes 660')
+print(blue+'Telegarm Grop : https://t.me/realtips6600')
+print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('''
+[1] Update Tool
+[2] WPS HACK
+[3] WPA2/PSK ATTCK
+[4] WIFI Jaming
+[0] EXIT
+  ''')
+a=input('Selected Option-:')
+if a=='1':
+	os.system('apt install figlet -y')
+	os.system('apt install ruby -y')
+	os.system('gem install lolcat')
+	os.system('python3 install.py')
+if a=='2':
+	os.system('clear')
+	os.system('termimage wps.jpg')
+	print(green+'Root chack.......')
+	os.system('sudo python wps.py -i wlan0 -K')
+	are=str(input(cyan+'Are you continue (y/n):- '))
+	if are=='y':
+		os.system('python3 wifi.py')
+	elif are=='n':
+		os.system('exit')
+		os.system('clear')
+		os.system('figlet -f big Thanks |lolcat')
+	elif are=='y ':
+		os.system('python3 wifi.py')
+	elif are=='n ':
+		os.system('exit')
+		os.system('clear')
+		os.system('figlet -f big Thanks |lolcat')
+if a=='3':
+	os.system('clear')
+	os.system('clear')
+	os.system('figlet -f big I -   Tracker |lolcat')
+	print(green+'                     Version : 1.0 ')
+	print()
+	print(blue+'======================================================')
+	print(red+'Note: I wont be responsible fo any illigal activites.')
+	print(blue+'======================================================')
+	print()
+	#main.coding
+	#________________________________________
+	import os
+	import sys
+	import time
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(green+'%37s' % '   |Devoloped By : Raz Ahamed|\n')
+	print()
+	print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	print(blue+'Youtube Chanel: Real  tips and Trikes 660')
+	print(blue+'Telegarm Grop : https://t.me/realtips6600')
+	print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	print('[1] Monitor Wifi')
+	print('[2] Captured Handshake')
+	print('[3] Hs File crack')
+	print('')
+	print('')
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(green+'Coming soon.......')
+	os.system('python3 wifi.py')
+if a=='3':
+	os.system('clear')
+	os.system('clear')
+	os.system('figlet -f big I -   Tracker |lolcat')
+	print(green+'                     Version : 1.0 ')
+	print()
+	print(blue+'======================================================')
+	print(red+'Note: I wont be responsible fo any illigal activites.')
+	print(blue+'======================================================')
+	print()
+	#main.coding
+	#________________________________________
+	import os
+	import sys
+	import time
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(green+'%37s' % '   |Devoloped By : Raz Ahamed|\n')
+	print()
+	print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	print(blue+'Youtube Chanel: Real  tips and Trikes 660')
+	print(blue+'Telegarm Grop : https://t.me/realtips6600')
+	print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	print('[1] Monitor Wifi')
+	print('[2] Captured Handshake')
+	print('[3] Hs File crack')
+	print('')
+	print('')
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(green+'Coming soon.......')
+	os.system('python3 wifi.py')
+elif a=='0':
+		os.system('exit')
+		print()
+		def delay_print(s):
+		    for c in s:
+		        sys.stdout.write(c)
+		        sys.stdout.flush()
+		        time.sleep(0.10)
+		delay_print(red+'EXIT..........')
+		os.system('clear')
+		os.system('figlet -f big Thanks')
+		
+			
+else:
+	print()
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(red+'Wrong Input Agin!')
+	os.system('python3 wifi.py')
+	
